24896f48a4860f8483bfacb36768638fce7b6021
AlaaEzzeldin <khalila@fim.uni-passau.de>
2021-08-08 00:47:47 +0200

    fix the inconsistent coordinates

--- getWeather.py
+++ getWeather.py
@@ -13,17 +13,24 @@ with open('hikes_rate5.csv', 'r') as file:
     with open('hikes_rate5_weather_7days.json', 'w', encoding='utf-8') as f:
         reader = csv.reader(file)
         i = 0
+        print(i)
+
         for row in reader:
             latitude = row[3]
             longitude = row[4]
+            print(i)
             response = requests.get("http://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutely,alerts" + "&appid=" + apiKey)
+            print(response)
+
             if json.loads(response.text):
                 data = json.loads(response.text)
                 weather[i]['weather'] = data
             if i % 50 == 0:  # since the API allows only for 60 requests/ min
                 print("batch "+str(i)+" is done")
-                time.sleep(60)
+                time.sleep(1)
             i += 1
+        print(i)
+
         json.dump(weather, f, ensure_ascii=False, indent=4)
         f.close()
         f2.close()
