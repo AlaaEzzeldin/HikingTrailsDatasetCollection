0b9d9366bd963f6f5d98cd7716cf886616ec0e6a
AlaaEzzeldin <khalila@fim.uni-passau.de>
2021-08-07 21:27:50 +0200

    fix the miss retrieved coordinates

--- getWeather.py
+++ getWeather.py
@@ -10,19 +10,19 @@ f2 = open('hikes_rate5.json',)
 test_data = json.load(f2)
 weather = test_data
 with open('hikes_rate5.csv', 'r') as file:
-    with open('hikes_rate5_weather.json', 'w', encoding='utf-8') as f:
+    with open('hikes_rate5_weather_7days.json', 'w', encoding='utf-8') as f:
         reader = csv.reader(file)
         i = 0
         for row in reader:
             latitude = row[3]
-            longitude = row[4   ]
-            response = requests.get(
-                "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey)
+            longitude = row[4]
+            response = requests.get("http://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutely,alerts" + "&appid=" + apiKey)
             if json.loads(response.text):
                 data = json.loads(response.text)
                 weather[i]['weather'] = data
-            if i % 4 == 0:  # since the API allows only for 60 requests/ min
-               time.sleep(5)
+            if i % 50 == 0:  # since the API allows only for 60 requests/ min
+                print("batch "+str(i)+" is done")
+                time.sleep(60)
             i += 1
         json.dump(weather, f, ensure_ascii=False, indent=4)
         f.close()
